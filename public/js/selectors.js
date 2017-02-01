53b911c009ffcc6d0edf325e09696c2f044e32d9
Alexia Young <alexia.kwok@gmail.com>
2017-02-01 10:49:36 -0600

    removable markers!

--- public/js/selectors.js
+++ public/js/selectors.js
@@ -3,28 +3,34 @@ const Makeday = function(){
     this.hotel = {};
     this.restaurants = {};
     this.activities = {};
-}
+};
 
 Makeday.prototype.addHotel = function(hotelObj, marker){
     this.hotel[hotelObj.name] = [hotelObj, marker];
 };
 
-Makeday.prototype.addRestaurant = function(resName, res, marker){
-    this.restaurants[resName] = [res, marker];
+Makeday.prototype.addRestaurant = function(resObj, marker){
+    this.restaurants[resObj.name] = [resObj, marker];
 };
 
-Makeday.prototype.addActivities = function(actName, act, marker){
-    this.activities[actName] = [act, marker];
+Makeday.prototype.addActivities = function(actObj, marker){
+    this.activities[actObj.name] = [actObj, marker];
 };
 
-Makeday.prototype.removeMaker = function(name,type ) {
+Makeday.prototype.removeMaker = function(name, type ) {
     var latlon = this[type][name].place.location;
 
-}
+};
+
+Makeday.prototype.removeRestaurant = function(resName) {
+  delete this.restaurants[resName];
+};
+
 
 var dayOne = new Makeday();
 
 var currentDay = dayOne;   //pointer
+
 
 $(document).ready(function(){
 //hotels
@@ -37,7 +43,6 @@ $(document).ready(function(){
         currentDay.addHotel(selectedHotel,marker);
         $('#hotel-list').append( '<li>' + selectedHotel.name + '</li><button class="btn btn-xs btn-danger remove btn-circle">x</button>');
        }
-
     });
 
 
@@ -45,20 +50,21 @@ $(document).ready(function(){
     $('#add-restaurant').click(function() {
       let restaurantIndex = $('#restaurant-choices option:selected').val() -1;
       let selectedRestaurant = restaurants[restaurantIndex];
-      let restaurantName = selectedRestaurant.name;
-      let restaurantObj = dayCollection[currentDay].restaurants;
+      let restaurantObj = currentDay.restaurants;
 
-      if(Object.keys(restaurantObj).length < 3 && !restaurantObj.hasOwnProperty(restaurantName)) {
-        let marker = drawMarker('restaurant', selectedRestaurant.place.location)
-        restaurantObj[restaurantName] = [selectedRestaurant, marker];
-        $('#restaurant-list').append('<li>' + restaurantName + '</li><button class="btn btn-xs btn-danger remove btn-circle">x</button>');
+      if(Object.keys(restaurantObj).length < 3 && !restaurantObj.hasOwnProperty(selectedRestaurant.name)) {
+        let marker = drawMarker('restaurant', selectedRestaurant.place.location);
+        currentDay.addRestaurant(selectedRestaurant, marker);
+        $('#restaurant-list').append('<li>' + selectedRestaurant.name + '</li><button class="btn btn-xs btn-danger remove btn-circle">x</button>');
       }
     });
 
     $( "#restaurant-list" ).on( "click", "button", function( event ) {
     //event.preventDefault();
     var removedName = $(this).prev()[0].innerHTML;
-
+    let removeMarker = currentDay.restaurants[removedName][1];
+    removeMarker.setMap(null);
+    currentDay.removeRestaurant(removedName);
     //console.log( , "logging" );
 
 });
@@ -68,14 +74,12 @@ $(document).ready(function(){
     $('#add-activity').click(function() {
       let activityIndex = $('#activity-choices option:selected').val() -1;
       let selectedActivity = activities[activityIndex];
-      let activityName = selectedActivity.name;
-      let activityObj = dayCollection[currentDay].activities;
+      let activityObj = currentDay.activities;
 
-      if(Object.keys(activityObj).length < 5 && !activityObj.hasOwnProperty(activityName)) {
+      if(Object.keys(activityObj).length < 5 && !activityObj.hasOwnProperty(selectedActivity.name)) {
         let marker = drawMarker('activity', selectedActivity.place.location);
-        activityObj[activityName] = [selectedActivity, marker];
+        currentDay.addActivities(selectedActivity, marker);
         $('#activity-list').append('<li>' + activityName + '</li>');
-
       }
     });
 
